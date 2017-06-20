66f47207dd79a7b787661323e67505693308278c
Shashi Gowda <shashi@juliacomputing.com>
2017-06-21 00:14:39 +0530

    initial niceties and layout

--- src/CSSUtil.jl
+++ src/CSSUtil.jl
@@ -1,5 +1,66 @@
 module CSSUtil
 
-# package code goes here
+export style, empty
+
+using WebIO
+using JSON
+import WebIO: render, Node
+
+using Measures
+
+function style(dict::Dict)
+    Dict(:style=>dict)
+end
+
+function style(p::Pair...)
+    style(Dict(p...))
+end
+
+function style(elem, dict::Dict)
+    render(elem)(style(dict))
+end
+
+function style(elem, p::Pair...)
+    style(elem, style(p...))
+end
+
+function style(::Void, arg...)
+    style(arg...)
+end
+
+const empty = dom"div"()
+
+export mm, em, cm, inch, pt, px, vw, vh, vmin, cent
+"1mm"
+const mm = Length(:mm, 1.0)
+" 1em "
+const em = Length(:em, 1.0)
+"1cm"
+const cm = Length(:cm, 1.0)
+"1inch"
+const inch = Length(:in, 1.0)
+"1pt"
+const pt = Length(:pt, 1.0)
+"1px"
+const px = Length(:px, 1.0)
+"1vw"
+const vw = Length(:vw, 1.0)
+"1vh"
+const vh = Length(:vh, 1.0)
+"1vmin"
+const vmin = Length(:vmin, 1.0)
+"1% length"
+const cent = Length(:cent, 1.0)
+
+JSON.lower{u}(l::Length{u}) = "$(l.value)$u"
+JSON.lower(l::Length{:cent}) = "$(l.value)%"
+
+function assertoneof(x, xs, name="argument")
+    if !(string(x) in xs)
+        throw(ArgumentError("$name should be one of $(join(xs, ",", "or")) as a string or symbol."))
+    end
+end
+
+include("layout.jl")
 
 end # module
---
+++ src/layout.jl
@@ -0,0 +1,95 @@
+export width, height, maxwidth, minwidth, maxheight, minheight,
+       boxsize, hbox, vbox, hskip, vskip, floating, wrap, pad,
+       padinner, alignitems, aligncontent, justifycontent, clip,
+       alignself, floating
+
+for (fn, prop) in [:width => :width,
+                   :height => :height,
+                   :minwidth => "min-width",
+                   :maxwidth => "max-width",
+                   :minheight => "min-height",
+                   :maxheight => "max-height",
+                  ]
+    @eval function $fn(val, elem=nothing)
+        style(elem, $prop => val)
+    end
+end
+
+function boxsize(w, h, elem=nothing)
+    style(elem, "width"=>w, "height"=>h)
+end
+
+function flex(elem=nothing)
+    style(elem, "display"=>"flex")
+end
+
+function container(xs...)
+    dom"div"(xs...)
+end
+
+function hbox(elems::AbstractVector)
+    container(map(render, elems))(style("display" => "flex", "flex-direction"=>"row"))
+end
+hbox(xs...) = hbox([xs...])
+
+function vbox(elems::AbstractVector)
+    container(map(render, elems))(style("display" => "flex", "flex-direction"=>"column"))
+end
+vbox(xs...) = vbox([xs...])
+
+hskip(x) = boxsize(x, 0px, empty)
+vskip(y) = boxsize(0px, y, empty)
+
+function wrap(elem=nothing; reverse=false)
+    style(elem, "flex-wrap" => reverse ? "wrap-reverse" : "wrap")
+end
+
+function floating(direction, elem=nothing)
+    style(elem, float=direction)
+end
+
+function justifycontent(justification, elem=nothing)
+    style(elem, "justify-content" => justification)
+end
+
+function alignitems(alignment, elem=nothing)
+    style(elem, "align-items" => alignment)
+end
+
+function alignself(alignment, elem=nothing)
+    style(elem, "align-self" => alignment)
+end
+
+function aligncontent(alignment, elem=nothing)
+    style(elem, "align-content" => alignment)
+end
+
+function _pad(side::Union{String,Symbol}, length)
+    assertoneof(side, ["top", "bottom", "left", "right"], "side")
+    "padding-$side" => length
+end
+
+function padinner(side::Union{String,Symbol}, length, elem=nothing)
+    style(elem, _pad(side, length))
+end
+
+function padinner(sides::Union{Vector, Tuple}, length, elem=nothing)
+    style(elem, (_pad(side, length) for side in sides)...)
+end
+
+function padinner(length, elem)
+    style(elem, "padding" => length)
+end
+
+function pad(sides, length, elem)
+    padinner(sides, length, container(elem))
+end
+
+function pad(length, elem)
+    padinner(length, container(elem))
+end
+
+function clip(overflow, elem)
+    assertoneof(overflow, ["hidden", "visible", "scroll", "auto"], "overflow")
+    style(container(elem), "overflow" => overflow)
+end
